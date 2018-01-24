b48a8d65030506120b41f599b418b7f0472e2a76
Rishab Hegde <rishabhegde@Rishabs-MacBook-Pro.local>
2018-01-24 02:20:13 -0800

    final commit?

--- contracts/PonziCoin.sol
+++ contracts/PonziCoin.sol
@@ -4,7 +4,7 @@ import "./StandardToken.sol";
 import "./SafeMath.sol";
 
 /// @title Token contract - Implements Standard Token Interface but adds Pyramid Scheme Support :)
-/// @author Rishab Hegde - <madoff@ponzischemeasaservice.com>
+/// @author Rishab Hegde - <contact@rishabhegde.com>
 contract PonziCoin is StandardToken, SafeMath {
 
     /*
@@ -14,13 +14,9 @@ contract PonziCoin is StandardToken, SafeMath {
     string constant public symbol = "SEC";
     uint8 constant public decimals = 3;
 
-    uint public buyPrice = 10000 szabo;
-    uint public sellPrice = 2500 szabo;
-    uint public tokensIssued = 0;
-    uint public tokensRedeemed = 0; 
-    uint public peopleActuallyPutThisMuchIntoALiteralPonziScheme = 0;
-    uint public currentWalletBalance = 0;
-    uint public tierBudget = 100;
+    uint public buyPrice = 10 szabo;
+    uint public sellPrice = 2500000000000 wei;
+    uint public tierBudget = 100000;
 
     // Address of the founder of PonziCoin.
     address public founder = 0x9acBAE8ece4E82DDAecca4cd78A148E137356d50;
@@ -41,40 +37,32 @@ contract PonziCoin is StandardToken, SafeMath {
       }
       
       uint investment = tokenCount * buyPrice;
-      if (msg.value > investment && !msg.sender.send(msg.value - investment)) {
-        throw;
-      }
 
-      peopleActuallyPutThisMuchIntoALiteralPonziScheme += investment;
-      currentWalletBalance += investment;
       balances[msg.sender] += tokenCount;
       Issuance(msg.sender, tokenCount);
-      tokensIssued += tokenCount;
       totalSupply += tokenCount;
       tierBudget -= tokenCount;
 
       if (tierBudget <= 0) {
-        tierBudget = 100;
+        tierBudget = 100000;
         buyPrice *= 2;
         sellPrice *= 2;
+      }
+      if (msg.value > investment) {
+        msg.sender.transfer(msg.value - investment);
       }
       return true;
     }
 
     function withdraw(uint tokenCount)
       public
-      payable
       returns (bool)
     {
       if (balances[msg.sender] >= tokenCount) {
         uint withdrawal = tokenCount * sellPrice;
-        if (!msg.sender.send(withdrawal)) {
-          throw;
-        }
         balances[msg.sender] -= tokenCount;
-        tokensRedeemed += tokenCount;
         totalSupply -= tokenCount;
-        currentWalletBalance -= withdrawal;
+        msg.sender.transfer(withdrawal);
         return true;
       } else {
         return false;
@@ -84,9 +72,8 @@ contract PonziCoin is StandardToken, SafeMath {
     /// @dev Contract constructor function sets initial token balances.
     function PonziCoin()
     {   
-
         // It's not a good scam unless it's pre-mined
-        balances[founder] = 100;
-        totalSupply += 100;
+        balances[founder] = 200000;
+        totalSupply += 200000;
     }
 }
